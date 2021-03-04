1536eb2894f03e922a4db7d5b9edb9bd871c30ec
pepe1973 <bodrogipr@gmail.com>
2021-03-04 10:25:28 +0100

    Projekt létrehozása

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FileKezeles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
